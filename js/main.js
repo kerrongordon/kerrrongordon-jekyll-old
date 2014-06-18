d2adcebbe8f10e42bfabfb449622d09c8dd4944d
kerrongordon <kgpsounds.com@gmail.com>
2014-06-18 18:02:46 -0400

    update portfolio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
     //$("body").fadeIn(2000);
+    
+    $('.more-infor').click(function() {
+        $('.site-preview').addClass('show');
+        return false;
+    });
+    
+    $('.close-btn').click(function() {
+        $('.site-preview').removeClass('show');
+        return false;
+    });
 });
    
         
