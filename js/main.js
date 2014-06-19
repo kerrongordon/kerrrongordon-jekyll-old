da349293eafbc9a1aac0e781e3bf9e011ac955e9
kerrongordon <kgpsounds.com@gmail.com>
2014-06-18 23:10:06 -0400

    add ajax
    // Browser Specific
    -webkit-filter: blur(5px);
    -moz-filter: blur(5px);
    -o-filter: blur(5px);
    -ms-filter: blur(5px);

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 $( document ).ready(function() {
     //$("body").fadeIn(2000);
     
-    $('.more-infor').click(function() {
+     $('.load').load('protfolio/dis/protfolio.html');
+    
+    
+    $('body').on('click','.more-infor', function() {
         $('.site-preview').addClass('show');
+        $('.load').addClass('slide-out');
         return false;
     });
     
-    $('.close-btn').click(function() {
+    $('body').on('click', '.close-btn',function() {
         $('.site-preview').removeClass('show');
+        $('.load').removeClass('slide-out');
         return false;
     });
+    
+    $('body').on('click','.more-infor',function(event) {
+        var pageInfor = $(this).attr('id');
+            $('.site-preview').load('protfolio/full/' + pageInfor + '.html');
+            return false;
+    });
+    
+   
 });
    
         
