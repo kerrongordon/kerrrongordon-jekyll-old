4e4e1483ece1e9e0c3868777719bd359b8b33630
kerrongordon <kgpsounds.com@gmail.com>
2014-09-22 20:08:01 -0400

    update home page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,13 +12,4 @@ $(document).ready(function() {
     });
   });
 
-  // underline under the active nav item
-  $(".nav .nav-link").click(function() {
-    $(".nav .nav-link").each(function() {
-      $(this).removeClass("active-nav-item");
-    });
-    $(this).addClass("active-nav-item");
-    $(".nav .more").removeClass("active-nav-item");
-  });
-
 });
