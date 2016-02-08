568413fe1ff7f308eb5a9753b6e6b6be291fbbf3
kerrongordon <kgpsounds.com@gmail.com>
2016-02-07 22:16:15 -0400

    fix contact form and add notification

--- js/founction.js
+++ js/founction.js
@@ -1,10 +1,9 @@
-'use strict';
-
 $(document).ready(function () {
 
   pageScroll(800);
-  mainMenu(); 
+  mainMenu();
   projects(500);
+  forms('kgpsounds.com@gmail.com');
 
 });
 
@@ -17,7 +16,7 @@ var pageScroll = (function (speed) {
   function goToSection(e) {
     var targ    = $(this).attr('href'),
         target  = targ.replace('/', '');
- 
+
       $('html, body').animate({
         scrollTop: $(target).offset().top
       }, speed);
@@ -45,7 +44,7 @@ var mainMenu = (function () {
       if (body.hasClass('menu-open')) {
         body.removeClass( "menu-open" );
         navToggle.removeClass( "active" );
-      };
+      }
     }
 
 });
@@ -88,5 +87,53 @@ var projects = (function(animat){
             next();
         });
     }
+
+});
+
+
+var forms = (function (email) {
+  var form         = $('#contact__form'),
+      formInput    = $('.contact__form__input');
+
+    form.submit(submitEmail);
+
+    function submitEmail(e) {
+      e.preventDefault();
+      $.ajax({
+    		url: '//formspree.io/' + email,
+    		method: 'POST',
+    		data: $(this).serialize(),
+    		dataType: 'json',
+    		beforeSend: function() {
+          formInput.val('');
+    		},
+    		success: function(data) {
+          notification('ms');
+    		},
+    		error: function(err) {
+          notification('e');
+    		}
+    	});
+    }
+});
+
+
+var notification = (function (e) {
+  var notification      = $('.notification');
+
+  notification.addClass('notification__show');
+
+  if (e === 'ms') {
+    notification.append('<p class="notification__notify notification__notify--success">Message sent!</p>');
+  } else if (e === 'e') {
+    notification.append('<p class="notification__notify notification__notify--error">Ops, there was an error.</p>');
+  }
+
+  notification.delay(5000)
+  .queue( function(next){
+      $(this).removeClass('notification__show');
+      $('.notification__notify').remove();
+      next();
+  });
 
 });
